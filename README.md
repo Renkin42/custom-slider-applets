d5a383ad123b5cd6679bcbaa4a0b2f3eb07ab599
Austin Leydecker <arleydecker@renkin42.net>
2024-05-11 21:46:13 -0700

    Switch to C

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # custom-slider-applets
 A simple generic GTK slider applet that executes user-defined shell commands
+Compile with
+```
+gcc $(pkg-config --cflags gtk4) -o hello-world-gtk hello-world-gtk.c $(pkg-config --libs gtk4)
+```
--- custom-slider/__main__.py
+++ custom-slider/__main__.py
@@ -10,9 +10,20 @@ APPINDICATOR_ID = 'myappindicator'
 def main():
     indicator = appindicator.Indicator.new(APPINDICATOR_ID, 'whatever', appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(gtk.Menu())
+    indicator.set_menu(build_menu())
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     gtk.main()
+
+def build_menu():
+    menu = gtk.Menu()
+    item_quit = gtk.MenuItem.new_with_label('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+ 
+def quit(source):
+    gtk.main_quit()
 
 if __name__ == "__main__":
     main()
