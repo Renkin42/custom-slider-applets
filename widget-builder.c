e38980cb275421b31d467454ebf04759f11e1492
Austin Leydecker <arleydecker@renkin42.net>
2024-05-16 06:07:28 -0700

    More building blocks

---
+++ widget-builder.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#include "widget-builder.h"
+
+static void
+add_new_button(GtkBox  *parent,
+               gchar   **label,
+               gchar   **cmd)
+{
+    //TODO
+}
---
+++ widget-builder.h
@@ -0,0 +1,9 @@
+#ifndef WIDGET_BUILDER_H
+#define WIDGET_BUILDER_H
+
+enum widget_type {
+    WIDGET_TYPE_BUTTON,
+    WIDGET_TYPE_SLIDER
+}
+
+#endif
