c4f9f35967a36ba89353bd6331860e967cf901c7
Austin Leydecker <arleydecker@renkin42.net>
2024-05-17 08:08:22 -0700

    Add widget data structs

--- widget-builder.h
+++ widget-builder.h
@@ -4,6 +4,21 @@
 enum widget_type {
     WIDGET_TYPE_BUTTON,
     WIDGET_TYPE_SLIDER
-}
+};
+
+struct button_data {
+    gchar *label;
+    gchar *cmd;
+};
+
+struct slider_data {
+    gchar *label;
+    gchar *get_cmd;
+    gchar *set_cmd;
+    gdouble *get_cmd_min;
+    gdouble *get_cmd_max;
+    gdouble *set_cmd_min;
+    gdouble *set_cmd_max;
+};
 
 #endif
