5637791b5540b862bc712b887397a66096b3df4e
Austin Leydecker <arleydecker@renkin42.net>
2024-05-17 23:05:53 -0700

    Add Button functions

--- widget-builder.c
+++ widget-builder.c
@@ -5,9 +5,23 @@
 #include "widget-builder.h"
 
 static void
-add_new_button(GtkBox  *parent,
-               gchar   **label,
-               gchar   **cmd)
+button_cmd_callback (GtkWidget *widget,
+                     gchar     *cmd)
 {
-    //TODO
+    system(cmd);
+}
+
+static void
+add_new_button (GtkBox             *parent,
+                struct button_data *data)
+{
+    GtkWidget *button;
+    gchar     *cmd;
+
+    strcpy(cmd, data->cmd)
+
+    button = gtk_button_new_with_label(data->label);
+    g_signal_connect(button, "clicked", G_CALLBACK (button_cmd_callback), cmd);
+
+    gtk_box_pack_end(parent, button, TRUE, TRUE, 0);
 }
